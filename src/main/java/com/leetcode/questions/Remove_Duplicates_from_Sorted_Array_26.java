6c6d88d6db272445a7bfb72364885926bb815eac
deepak <deepakjose770@gmail.com>
2024-05-21 20:28:49 +0530

    adding some code from personal PC

--- src/main/java/com/leetcode/questions/Remove_Duplicates_from_Sorted_Array_26.java
+++ src/main/java/com/leetcode/questions/Remove_Duplicates_from_Sorted_Array_26.java
@@ -1,25 +1,28 @@
 package com.leetcode.questions;
 
-public class Remove_Duplicates_from_Sorted_Array_26 {
+public class Remove_Duplicates_From_Sorted_Array_26 {
     public static int removeDuplicates(int[] nums) {
-        //https://leetcode.com/problems/remove-duplicates-from-sorted-array/solutions/3676877/best-method-100-c-java-python-beginner-friendly/
-        int j = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[j] = nums[i];
+        if (nums.length == 0) return 0; // Handle empty array
+
+        int j = 1; // pointer for next unique element position
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                nums[j] = nums[i]; // Copy unique element
                 j++;
             }
         }
-        return j;
+        return j; // This is the number of unique elements (length of the modified array)
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2, 2, 3, 4, 4};
-        int len = removeDuplicates(nums);
-        System.out.println("New length: " + len);
-        for (int i = 0; i < len; i++) {
-            System.out.print(nums[i] + " ");
+        int[] nums = {1, 1, 2, 3, 3, 4};
+        int uniqueCount = removeDuplicates(nums);
+        System.out.println("Unique elements: " + uniqueCount);
+        for (int i = 0; i < uniqueCount; i++) {
+            System.out.print(nums[i] + " "); // Print the modified array (first 'uniqueCount' elements)
         }
     }
+
 }
+
 
