bfc234bdd9ac718990f4c6a4e33fa25e9d68913e
Deepak A JOSE <deepak.ajose@amadeus.com>
2024-05-27 13:15:53 +0530

    Adding from office PC

--- src/main/java/com/deepak/blind75/Longest_Consecutive_Sequence_128.java
+++ src/main/java/com/deepak/blind75/Longest_Consecutive_Sequence_128.java
@@ -3,10 +3,6 @@ package com.deepak.blind75;
 public class Longest_Consecutive_Sequence_128 {
     public static void main(String[] args) {
         int[] nums = {100, 4, 200, 1, 3, 2};
-        System.out.println(longestConsecutive(nums));
-    }
-
-    private static int longestConsecutive(int[] nums) {
-        
+        //System.out.println(longestConsecutive(nums));
     }
 }
---
+++ src/main/java/com/deepak/blind75/ThreeSum_15.java
@@ -0,0 +1,42 @@
+package com.deepak.blind75;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ThreeSum_15 {
+    public static void main(String[] args) {
+        int[] nums  = {-1, 0, 1, 2, -1, -4, 5};
+        List<List<Integer>> result = threeSum(nums);
+        System.out.println(result);
+    }
+
+    private static List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null ||
+                nums.length < 3)
+            return new ArrayList<>();
+
+        Arrays.sort(nums);
+        Set<List<Integer>> resultSet = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i+1;
+            int right = nums.length-1;
+
+            while (left < right) {
+                int sum  = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    resultSet.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    right--;
+                    left++;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(resultSet);
+    }
+}
