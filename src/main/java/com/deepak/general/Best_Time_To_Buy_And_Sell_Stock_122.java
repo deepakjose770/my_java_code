6494125e94f1abc3c09c17a077cb970782af39eb
Deepak A JOSE <deepak.ajose@amadeus.com>
2024-09-06 10:26:09 +0530

    Adding from office PC

--- src/main/java/com/deepak/general/Best_Time_To_Buy_And_Sell_Stock_122.java
+++ src/main/java/com/deepak/general/Best_Time_To_Buy_And_Sell_Stock_122.java
@@ -5,7 +5,6 @@ public class Best_Time_To_Buy_And_Sell_Stock_122 {
         int[] prices = {7,1,5,3,6,4};
         System.out.println("Result "+ new Best_Time_To_Buy_And_Sell_Stock_122().maxProfit(prices));
     }
-
     public int maxProfit(int[] prices) {
         int totalProfit = 0;
         for (int i = 1; i<prices.length; i++) {
@@ -13,5 +12,4 @@ public class Best_Time_To_Buy_And_Sell_Stock_122 {
         }
         return totalProfit;
     }
-
 }
