b471b31cde24c5a95770853af6fbb3de33c682f4
deepakjose770 <deepakjose770@gmail.com>
2024-12-01 11:53:31 +0530

    Committing from personal PC

--- src/main/java/com/deepak/general/Rotate_List_61.java
+++ src/main/java/com/deepak/general/Rotate_List_61.java
@@ -2,8 +2,42 @@ package com.deepak.general;
 
 import com.deepak.util.ListNode;
 
-public class Rotate_List_61 {
-    public ListNode rotateRight(ListNode head, int k) {
+import java.util.List;
 
+public class Rotate_List_61 {
+
+    public static void main(String[] args) {
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);
+        ListNode result1 = new Rotate_List_61().rotateRight(head1, 2);
+    }
+
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k ==0) {
+            return head;
+        }
+
+        int listLength = 1;
+        ListNode tempNode = head;
+        while (tempNode.next!= null) {
+            tempNode = tempNode.next;
+            listLength++;
+        }
+
+        int effectiveRotation = k % listLength;
+        int newTailLocation = listLength - effectiveRotation - 1;
+        tempNode.next = head;
+
+        tempNode = head;
+        for (int i = 0; i < newTailLocation; i++) {
+            tempNode = tempNode.next;
+        }
+        head = tempNode.next;
+        tempNode.next = null;
+        return head;
     }
 }
